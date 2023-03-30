b4cbb7de5475dae4fc53273c6ac6173dafae6155
Lorenz Schreyer <lorenz.schreyer@gmail.com>
2023-03-30 22:38:01 +0200

    First approach for rest api calls

---
+++ backend/codingchallenge/api/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Test
+class TestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Test
+        fields = ["task", "timestamp"]
---
+++ backend/codingchallenge/api/urls.py
@@ -0,0 +1,10 @@
+# from django.conf.urls import url
+from django.urls import path, include
+
+from .views import (
+    TestListApiView,
+)
+
+urlpatterns = [
+    path('task', TestListApiView.as_view()),
+]
