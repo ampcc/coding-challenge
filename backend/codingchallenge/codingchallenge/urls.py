004769e49c60cd1e1c8f4788bfc8caed11edf172
Lorenz Schreyer <lorenz.schreyer@gmail.com>
2023-04-11 13:52:19 +0200

    excluded api from installed apps

--- backend/codingchallenge/codingchallenge/urls.py
+++ backend/codingchallenge/codingchallenge/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from api import urls as api_urls
+from backend.codingchallenge.api import urls as api_urls
 
 urlpatterns = [
     path('admin-panel/', admin.site.urls),
