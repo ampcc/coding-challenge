9aaf9e1cca67e18081329c98f10c30f9dfa743e2
Virginia <vis9494@thi.de>
2023-04-11 19:24:19 +0200

    Layout for start page

--- frontend/coding-challenge/src/app/app-routing.module.ts
+++ frontend/coding-challenge/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from './shared/components/start/start.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'start', component: StartComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/coding-challenge/src/app/shared/components/start/start.component.ts
+++ frontend/coding-challenge/src/app/shared/components/start/start.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
-  styleUrls: ['./start.component.css']
+  styleUrls: ['./start.component.css'],
+  standalone: true,
+  imports: [MatButtonModule],
 })
 export class StartComponent {
 
