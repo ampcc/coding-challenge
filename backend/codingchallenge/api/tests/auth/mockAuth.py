441974ac328798c697f6fa711861cb895533569b
Lorenz Schreyer <lorenz.schreyer@gmail.com>
2023-04-18 18:31:36 +0200

    Created mockAuth class for testing purposes #82

---
+++ backend/codingchallenge/api/tests/auth/mockAuth.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+
+class MockAuth:
+    @staticmethod
+    def admin(testcase):
+        # Authorisation Header
+        user = User.objects.create_superuser('admin', 'admin@test.com', "admin")
+        user.save()
+        token = Token.objects.create(user=user)
+        testcase.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+    @staticmethod
+    def applicant(testCase):
+        return 0
