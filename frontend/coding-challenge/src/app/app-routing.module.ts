e92b157910fe33d078b2440ca132aec257bc3912
Virginia <vis9494@thi.de>
2023-04-19 17:35:35 +0200

    Fixed duplicate component

--- frontend/coding-challenge/src/app/app-routing.module.ts
+++ frontend/coding-challenge/src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './shared/components/admin/admin.component';
 import { ChallengeComponent } from './shared/components/challenge/challenge.component';
 import { StartComponent } from './shared/components/start/start.component';
-import { ChallengeComponent } from './shared/components/challenge/challenge.component';
 
 const routes: Routes =
   [{ path: 'start', component: StartComponent },
-  { path: 'challenge', component: ChallengeComponent }{
-  path: 'admin', component: AdminComponent},
-  {path: 'challenge', component: ChallengeComponent
-}];
+  { path: 'challenge', component: ChallengeComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'challenge', component: ChallengeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
