4d747741eadec39d7e34ccc37530ee3c67f648b4
Virginia <vis9494@thi.de>
2023-04-22 20:14:02 +0200

    new component to edit challenges

---
+++ frontend/coding-challenge/src/app/shared/components/admin-edit/admin-edit.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminEditComponent } from './admin-edit.component';
+
+describe('AdminEditComponent', () => {
+  let component: AdminEditComponent;
+  let fixture: ComponentFixture<AdminEditComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdminEditComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AdminEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
