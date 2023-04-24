92c74406ddbd3f77fbac1d010d6accc76ac131a4
ara6585 <ara6585@thi.de>
2023-04-24 22:47:33 +0200

    updated admin /deleteChallenge and added tests

---
+++ backend/codingchallenge/api/tests/views/challenge/test_deleteChallengeAdmin.py
@@ -0,0 +1,25 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from ...auth.mockAuth import MockAuth
+from ....models.challenge import Challenge
+from ....views import jsonMessages
+
+class test_deleteChallengeAdmin(APITestCase):
+    url = "/api/admin/challenges/"
+    
+    def setUp(self):
+        # Authorization
+        MockAuth.admin(self)
+
+        Challenge.objects.create(challengeHeading="Test", challengeText="Text of challenge...")
+
+    def test_defaultRequest(self):
+        response = self.client.delete(self.url + "1/")
+        self.assertEqual(response.data, jsonMessages.successJsonResponse())
+
+    def test_tryDeleteOnNonexistentChallenge(self):
+        response = self.client.delete(self.url + "2/")
+        self.assertEqual(response.data, jsonMessages.errorJsonResponse("No object found for given challengeId."),
+                         status.HTTP_404_NOT_FOUND)
+    
--- backend/codingchallenge/api/views/challenges.py
+++ backend/codingchallenge/api/views/challenges.py
@@ -101,12 +101,17 @@ class AdminChallengesView(APIView):
 
     # /api/admin/challenges/<challengeId>
     def delete(self, request, *args, **kwargs):
-        challenge = Challenge.objects.filter(id=self.kwargs["challengeId"]).first()
         try:
-            challenge.delete()
-            return Response(jsonMessages.successJsonResponse(), status=status.HTTP_200_OK)
-        except:
-            return Response(jsonMessages.errorJsonResponse("Could not delete challenge " + self.kwargs["challengeId"] + "!"), status=status.HTTP_404_NOT_FOUND)
+            challenge = Challenge.objects.get(id=self.kwargs["challengeId"])
+        except Challenge.DoesNotExist:
+            return Response(jsonMessages.errorJsonResponse("No object found for given challengeId."), status=status.HTTP_404_NOT_FOUND)
+        except Challenge.MultipleObjectsReturned:
+            return Response(jsonMessages.errorJsonResponse("There have been found multiple challenges for the given challengeId. " +
+                                                           "This should never be the case."), status=status.HTTP_409_CONFLICT)
+        
+        Challenge.delete(challenge)
+        return Response(jsonMessages.successJsonResponse(), status=status.HTTP_200_OK)
+
 
 
 # endpoint: /api/application/challenges/<applicationId>
