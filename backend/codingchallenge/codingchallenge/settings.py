2a80ea6f41103bad8b41371d6e9cd0aefc3cce28
ara6585 <ara6585@thi.de>
2023-04-26 22:49:32 +0200

    set encryption key as settings variable

--- backend/codingchallenge/codingchallenge/settings.py
+++ backend/codingchallenge/codingchallenge/settings.py
@@ -30,6 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
  
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
+ENCRYPTION_KEY = os.getenv('ENCRYPTION_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
