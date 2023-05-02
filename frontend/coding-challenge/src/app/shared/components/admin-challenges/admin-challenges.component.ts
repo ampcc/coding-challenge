3b593cc853cf606ff4e750bc5140392b7adc28c8
Wecker, Veronika# <vew7531@thi.de>
2023-05-03 00:45:45 +0200

    added html structure and css to the admin challenge view

--- frontend/coding-challenge/src/app/shared/components/admin-challenges/admin-challenges.component.ts
+++ frontend/coding-challenge/src/app/shared/components/admin-challenges/admin-challenges.component.ts
@@ -1,10 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import {BackendService} from 'src/app/core/backend.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
+import {NgFor, NgIf} from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Challenge } from '../../models/challenge';
 
 @Component({
+  standalone: true,
+  imports: [
+    NgFor,
+    NgIf,
+    FormsModule,
+  ],
   selector: 'app-admin-challenges',
   templateUrl: './admin-challenges.component.html',
-  styleUrls: ['./admin-challenges.component.css']
+  styleUrls: ['./admin-challenges.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class AdminChallengesComponent {
+  challenge: Challenge;
+
+  public hideContentActiveChallenges: boolean = false;
+
+  public hideFilterSelect: boolean = true;
+
+  public challengeArray: Challenge[] = [{challengeId: 0, challengeHeading: 'Challenge0',challengeText: 'rtulip68o5ezrthsgfehjzr7ik4k6ujzhrtbfdv hf,kzujtzrgrhtj'},
+                                        {challengeId: 1, challengeHeading: 'Challenge1',challengeText: 'bgdnhmjzk,uzjthrf ngjmzik6u75z6tgrfvd fhtjm,il6izjtuegfwdc fdgnhmt,il'}
+                                       ];
+ 
+ 
+  public constructor(private backend: BackendService, private router: Router, public dialog: MatDialog,) {
+    this.challenge = {challengeId: 0, challengeHeading: '',challengeText: ''};
+  }
+
+
+  public ngOnInit(): void {
+  }
+
+  public changeTab(id: string): void {
+    let elementActiveChallenge = <HTMLLabelElement>document.getElementById('tab_active_challenges');
+    let elementArchive = <HTMLLabelElement>document.getElementById('tab_archiv');
+
+    switch(id) {
+      case 'tab_active_challenges':
+        this.hideContentActiveChallenges = false;
+
+        elementActiveChallenge.setAttribute("style", "border-bottom: 2px solid black;");
+        elementArchive.setAttribute("style", "border-bottom: none;");
+        break;
+    } 
+  }
+
+  public showFilter(): void {
+    this.hideFilterSelect = !this.hideFilterSelect;
+  }
+
+  public toggleTreeView(id: string): void {
+    let element = document.getElementById(id);
+    if(element !== null && element !== undefined) {    
+      let parentElement = element.parentElement;
+      
+      if(parentElement !== null && parentElement !== undefined) {
+        parentElement.querySelector(".nested")!.classList.toggle("active");
+        element.classList.toggle("caret-down");
+      }
+    }
+  }
+
+  public checkboxChallengeChange(): void {
+
+  }
+
+
+   public openDialogActiveChallenges(challenge: Challenge): void {
+    DialogComponent.name;
+    let dialogRef = this.dialog.open(DialogComponent, {
+      data: {
+        title: challenge.challengeHeading,
+        description: {
+          details: challenge.challengeText
+        },
+        buttons: {
+          left: { title: 'Edit', look: 'primary' },
+          right: { title: 'Cancel', look: 'secondary' }
+        }
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.router.navigateByUrl("/challenge");
+      }
+    })
+
+    
+  }
+
 
 }
