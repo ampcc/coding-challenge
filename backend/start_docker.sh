3402735ca673a72a2643c7aff1807af3d14aa224
Gfud <75432315+Gfud@users.noreply.github.com>
2023-05-02 10:09:50 +0200

    Solve problem with the newline-encoding

--- backend/start_docker.sh
+++ backend/start_docker.sh
@@ -2,7 +2,7 @@
 echo "Starting the container for the django server!\n"
 read -p "Do you want to build a new image? (This process could take a while) [Y/N]: " input
 
-if [ $(input) = 'Y' ]; then
+if [ $input = 'Y' ]; then
     docker build -t django-server:latest .
 fi
 docker run -p 8000:8000 --name django-server django-server:latest
--- frontend/start_docker.sh
+++ frontend/start_docker.sh
@@ -2,7 +2,7 @@
 echo "Starting the container for the angular website!\n"
 read -p "Do you want to build a new image? (This process could take a while) [Y/N]: " input
 
-if [ $(input) = 'Y' ]; then
+if [ $input = 'Y' ]; then
     docker build -t angular-website:latest .
 fi
 docker run -p 4200:4200 --name angular-website angular-website:latest
