44379a6afb41f2c4bda9d05502788b48952af671
Gfud <75432315+Gfud@users.noreply.github.com>
2023-05-05 15:12:38 +0200

    Add functionality to stop/delete containers/images

--- backend/start_docker.sh
+++ backend/start_docker.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
-echo "Starting the container for the django server!\n"
-read -p "Do you want to build a new image? (This process could take a while) [Y/N]: " input
+# This script starts the backend container and gives the possibility to delete all outdated images
 
-if [ $input = 'Y' ]; then
+read -p "Before the process starts: Do you want to delete all prior, unassociated images? (You can build a new image later) [Y/N]: " remove_input
+if [ $remove_input = 'Y' ]; then
+    docker image prune -a
+fi
+
+read -p "Do you want to build a new image? (This process could take a while) [Y/N]: " build_input
+if [ $build_input = 'Y' ]; then
     docker build -t django-server:latest .
 fi
 docker run -p 8000:8000 --name django-server django-server:latest
--- frontend/start_docker.sh
+++ frontend/start_docker.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
-echo "Starting the container for the angular website!\n"
-read -p "Do you want to build a new image? (This process could take a while) [Y/N]: " input
+# This script starts the frontend container and gives the possibility to delete all outdated images 
 
-if [ $input = 'Y' ]; then
+read -p "Before the process starts: Do you want to delete all prior, unassociated images? (You can build a new image later) [Y/N]: " remove_input
+if [ $remove_input = 'Y' ]; then
+    docker image prune -a
+fi
+
+read -p "Do you want to build a new image? (This process could take a while) [Y/N]: " build_input
+if [ $build_input = 'Y' ]; then
     docker build -t angular-website:latest .
 fi
 docker run -p 4200:4200 --name angular-website angular-website:latest
