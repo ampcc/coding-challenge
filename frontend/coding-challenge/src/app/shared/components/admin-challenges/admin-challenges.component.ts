b54dadefd992364a57e5d82289dbe1deda04666d
Fabian Bretz <vew7531@thi.de>
2023-05-22 21:03:28 +0200

    Add initial doc comments

--- frontend/coding-challenge/src/app/shared/components/admin-challenges/admin-challenges.component.ts
+++ frontend/coding-challenge/src/app/shared/components/admin-challenges/admin-challenges.component.ts
@@ -21,20 +21,18 @@ import { Challenge } from '../../models/challenge';
 })
 
 export class AdminChallengesComponent implements OnInit {
-  challenge: Challenge;
+  // challenge: Challenge;
   private adminToken: string | null;
 
   public hideContentActiveChallenges: boolean = false;
 
   public hideFilterSelect: boolean = true;
 
-  public challengeArray: Challenge[] = [{ id: 0, challengeHeading: 'Challenge0', challengeText: 'rtulip68o5ezrthsgfehjzr7ik4k6ujzhrtbfdv hf,kzujtzrgrhtj' },
-  { id: 1, challengeHeading: 'Challenge1', challengeText: 'bgdnhmjzk,uzjthrf ngjmzik6u75z6tgrfvd fhtjm,il6izjtuegfwdc fdgnhmt,il' }
-  ];
+  public challengeArray: Challenge[] = [];
 
 
   public constructor(private backend: BackendService, private router: Router, public dialog: MatDialog) {
-    this.challenge = { id: 0, challengeHeading: '', challengeText: '' };
+    // this.challenge = { id: 0, challengeHeading: '', challengeText: '' };
     this.adminToken = null;
   }
 
@@ -42,7 +40,7 @@ export class AdminChallengesComponent implements OnInit {
     // Check if Admin Token is available
     this.adminToken = window.sessionStorage.getItem('Adm-Token');
     if (this.adminToken === null) {
-      this.router.navigateByUrl("/admin_login")
+      this.router.navigateByUrl("/admin_login");
     } else {
       this.backend.getChallenges(this.adminToken).subscribe((response: Challenge[]) => {
         this.challengeArray = response;
@@ -50,6 +48,10 @@ export class AdminChallengesComponent implements OnInit {
     }
   }
 
+  /**
+   * 
+   * @param id 
+   */
   public changeTab(id: string): void {
     let elementActiveChallenge = <HTMLLabelElement>document.getElementById('tab_active_challenges');
     let elementArchive = <HTMLLabelElement>document.getElementById('tab_archiv');
@@ -64,32 +66,17 @@ export class AdminChallengesComponent implements OnInit {
     }
   }
 
+  /**
+   * Navigates the user to the designated page to add a challenge
+   */
   public addChallenge(): void {
     this.router.navigateByUrl("/admin_edit_challenge");
   }
 
-
-  // public showFilter(): void {
-  //   this.hideFilterSelect = !this.hideFilterSelect;
-  // }
-
-  // public toggleTreeView(id: string): void {
-  //   let element = document.getElementById(id);
-  //   if(element !== null && element !== undefined) {
-  //     let parentElement = element.parentElement;
-
-  //     if(parentElement !== null && parentElement !== undefined) {
-  //       parentElement.querySelector(".nested")!.classList.toggle("active");
-  //       element.classList.toggle("caret-down");
-  //     }
-  //   }
-  // }
-
-  // public checkboxChallengeChange(): void {
-
-  // }
-
-
+  /**
+   * 
+   * @param challenge 
+   */
   public openDialogActiveChallenges(challenge: Challenge): void {
     DialogComponent.name;
     let dialogRef = this.dialog.open(DialogComponent, {
@@ -121,6 +108,10 @@ export class AdminChallengesComponent implements OnInit {
   }
 
   // If a user decided to delete a challenge another dialog opens to ask for a second confirmation
+  /**
+   * 
+   * @param challenge 
+   */
   public openDeleteDialog(challenge: Challenge): void {
     let dialogRef = this.dialog.open(DialogComponent, {
       data: {
