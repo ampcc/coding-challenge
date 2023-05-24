5dc843c3e7de55b3c8164b411fd48ab0f277b518
Marcodaum <marco.daum@schanzer-racing.de>
2023-05-24 16:26:39 +0200

    added functionality for offline deployment

--- backend/codingchallenge/api/include/githubApi.py
+++ backend/codingchallenge/api/include/githubApi.py
@@ -1,8 +1,10 @@
-from github import Github
+from github import Github, GithubException
 from github.AppAuthentication import AppAuthentication
 import os
+from . import githubApiMockData
 from dotenv import load_dotenv
 from pathlib import Path
+from django.conf import settings
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 load_dotenv()
@@ -16,63 +18,85 @@ class GithubApi:
         self.installation_id = int(os.getenv('GH_APP_INSTALLATION_ID'))
         self.githubOrg = "ampcc"
 
-        self.gApi = Github(app_auth=AppAuthentication(app_id=self.app_id, private_key=self.private_key,
-                                                      installation_id=self.installation_id))
+        if not settings.DEPLOY_OFFLINE:
+            self.gApi = Github(app_auth=AppAuthentication(app_id=self.app_id, private_key=self.private_key,
+                                                        installation_id=self.installation_id))
 
     def getRepoUrl(self, repoName):
-        return self.gApi.get_organization(self.githubOrg).get_repo(repoName).url
+        if settings.DEPLOY_OFFLINE:
+            if repoName in githubApiMockData.getRepos:
+                return githubApiMockData.getRepoUrl(repoName)
+            else:
+                raise GithubException(400, {"message": "Repo not found!"}, None)
+        else:
+            return self.gApi.get_organization(self.githubOrg).get_repo(repoName).url
+
     def getRepos(self):
-        ret = []
-
-        for repo in self.gApi.get_organization(self.githubOrg).get_repos():
-            ret.append(repo.name)
-
-        return ret
+        if settings.DEPLOY_OFFLINE:
+            return githubApiMockData.getRepos
+        else:
+            ret = []
+            for repo in self.gApi.get_organization(self.githubOrg).get_repos():
+                ret.append(repo.name)
+            return ret
 
     def createRepo(self, repoName, repoDescription):
-        self.gApi.get_organization(self.githubOrg).create_repo(name=repoName, description=repoDescription, private=True)
-        return True
+        if settings.DEPLOY_OFFLINE:
+            return githubApiMockData.createRepo
+        else:
+            return self.gApi.get_organization(self.githubOrg).create_repo(name=repoName, description=repoDescription, private=True)
 
     def deleteRepo(self, repoName):
-        return self.gApi.get_organization(self.githubOrg).get_repo(repoName).delete()
+        if settings.DEPLOY_OFFLINE:
+            return githubApiMockData.deleteRepo
+        else:
+            return self.gApi.get_organization(self.githubOrg).get_repo(repoName).delete()
 
 
     # def pushFiles(self):
     def pushFile(self, repoName, path, file):
-        return self.gApi.get_organization(self.githubOrg).get_repo(repoName).create_file(path=path,
+        if settings.DEPLOY_OFFLINE:
+            return githubApiMockData.pushFile
+        else:
+            return self.gApi.get_organization(self.githubOrg).get_repo(repoName).create_file(path=path,
                                                                                   message="auto push " + path,
                                                                                   content=file)
 
     def addLinter(self, repoName):
-
-        return self.gApi.get_organization(self.githubOrg).get_repo(repoName).create_file(
-            path=".github/workflows/megalinter.yml",
-            message="added megalinter",
-            content=open(BASE_DIR.joinpath("api/include/megalinter.yml"), 'r').read())
-
+        if settings.DEPLOY_OFFLINE:
+            return githubApiMockData.addLinter
+        else:
+            return self.gApi.get_organization(self.githubOrg).get_repo(repoName).create_file(
+                path=".github/workflows/megalinter.yml",
+                message="added megalinter",
+                content=open(BASE_DIR.joinpath("api/include/megalinter.yml"), 'r').read())
 
     def getLinterLog(self, repoName):
-        return self.gApi.get_organization(self.githubOrg).get_repo(repoName).get_contents(
-            'megalinter-reports/megalinter.log').decoded_content.decode()
+        if settings.DEPLOY_OFFLINE:
+            return githubApiMockData.getLinterLog
+        else:
+            return self.gApi.get_organization(self.githubOrg).get_repo(repoName).get_contents(
+                'megalinter-reports/megalinter.log').decoded_content.decode()
 
     def getLinterResult(self, repoName):
+        if settings.DEPLOY_OFFLINE:
+            return githubApiMockData.getLinterResult
+        else:
+            decodedLinter = self.getLinterLog(repoName)
 
-        decodedLinter = self.getLinterLog(repoName)
+            # ----SUMMARY ----
+            linterStartIndex = decodedLinter.find("+----SUMMARY----")
+            linterSummary = decodedLinter[linterStartIndex:-1:1]
+            linterEndIndex = linterSummary.find('\n\n')
 
-        # ----SUMMARY ----
-        linterStartIndex = decodedLinter.find("+----SUMMARY----")
-        linterSummary = decodedLinter[linterStartIndex:-1:1]
-        linterEndIndex = linterSummary.find('\n\n')
+            cleanSummary = linterSummary[0:linterEndIndex:1]
 
-        cleanSummary = linterSummary[0:linterEndIndex:1]
+            # replacing symbols and added padding for correct spacing
+            # check
+            cleanSummary = cleanSummary.replace(u"\u2705",u"\u2713" + " ")
+            # cross
+            cleanSummary = cleanSummary.replace(u"\u274c",u"\u2715" + " ")
+            # question mark
+            cleanSummary = cleanSummary.replace(u"\u25EC",u"\u2047")
 
-        # replacing symbols and added padding for correct spacing
-        # check
-        cleanSummary = cleanSummary.replace(u"\u2705",u"\u2713" + " ")
-        # cross
-        cleanSummary = cleanSummary.replace(u"\u274c",u"\u2715" + " ")
-        # question mark
-        cleanSummary = cleanSummary.replace(u"\u25EC",u"\u2047")
-
-        return cleanSummary
-
+            return cleanSummary
