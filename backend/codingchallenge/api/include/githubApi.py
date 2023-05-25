f2004d49eb4b32c54e9ec5f855a115f494ae82ed
Marcodaum <marco.daum@schanzer-racing.de>
2023-05-25 13:24:46 +0200

    fixed rebase errors

--- backend/codingchallenge/api/include/githubApi.py
+++ backend/codingchallenge/api/include/githubApi.py
@@ -24,18 +24,8 @@ class GithubApi:
         if not settings.DEPLOY_OFFLINE:
             self.gApi = Github(app_auth=AppAuthentication(app_id=self.app_id, private_key=self.private_key,
                                                         installation_id=self.installation_id))
-        if not settings.DEPLOY_OFFLINE:
-            self.gApi = Github(app_auth=AppAuthentication(app_id=self.app_id, private_key=self.private_key,
-                                                        installation_id=self.installation_id))
 
     def getRepoUrl(self, repoName):
-        if settings.DEPLOY_OFFLINE:
-            if repoName in githubApiMockData.getRepos:
-                return githubApiMockData.getRepoUrl(repoName)
-            else:
-                raise GithubException(400, {"message": "Repo not found!"}, None)
-        else:
-            return self.gApi.get_organization(self.githubOrg).get_repo(repoName).url
         if settings.DEPLOY_OFFLINE:
             if repoName in githubApiMockData.getRepos:
                 return githubApiMockData.getRepoUrl(repoName)
@@ -53,29 +43,14 @@ class GithubApi:
             for repo in self.gApi.get_organization(self.githubOrg).get_repos():
                 ret.append(repo.name)
             return ret
-        if settings.DEPLOY_OFFLINE:
-            return githubApiMockData.getRepos
-        else:
-            ret = []
-            for repo in self.gApi.get_organization(self.githubOrg).get_repos():
-                ret.append(repo.name)
-            return ret
 
     def createRepo(self, repoName, repoDescription):
         if settings.DEPLOY_OFFLINE:
             return githubApiMockData.createRepo
         else:
             return self.gApi.get_organization(self.githubOrg).create_repo(name=repoName, description=repoDescription, private=True)
-        if settings.DEPLOY_OFFLINE:
-            return githubApiMockData.createRepo
-        else:
-            return self.gApi.get_organization(self.githubOrg).create_repo(name=repoName, description=repoDescription, private=True)
 
     def deleteRepo(self, repoName):
-        if settings.DEPLOY_OFFLINE:
-            return githubApiMockData.deleteRepo
-        else:
-            return self.gApi.get_organization(self.githubOrg).get_repo(repoName).delete()
         if settings.DEPLOY_OFFLINE:
             return githubApiMockData.deleteRepo
         else:
@@ -98,13 +73,6 @@ class GithubApi:
                 path=".github/workflows/megalinter.yml",
                 message="added megalinter",
                 content=open(BASE_DIR.joinpath("api/include/megalinter.yml"), 'r').read())
-        if settings.DEPLOY_OFFLINE:
-            return githubApiMockData.addLinter
-        else:
-            return self.gApi.get_organization(self.githubOrg).get_repo(repoName).create_file(
-                path=".github/workflows/megalinter.yml",
-                message="added megalinter",
-                content=open(BASE_DIR.joinpath("api/include/megalinter.yml"), 'r').read())
 
     def getLinterLog(self, repoName):
         if settings.DEPLOY_OFFLINE:
@@ -112,17 +80,8 @@ class GithubApi:
         else:
             return self.gApi.get_organization(self.githubOrg).get_repo(repoName).get_contents(
                 'megalinter-reports/megalinter.log').decoded_content.decode()
-        if settings.DEPLOY_OFFLINE:
-            return githubApiMockData.getLinterLog
-        else:
-            return self.gApi.get_organization(self.githubOrg).get_repo(repoName).get_contents(
-                'megalinter-reports/megalinter.log').decoded_content.decode()
 
     def getLinterResult(self, repoName):
-        if settings.DEPLOY_OFFLINE:
-            return githubApiMockData.getLinterResult
-        else:
-            decodedLinter = self.getLinterLog(repoName)
         if settings.DEPLOY_OFFLINE:
             return githubApiMockData.getLinterResult
         else:
@@ -142,34 +101,14 @@ class GithubApi:
             cleanSummary = cleanSummary.replace(u"\u274c", u"\u2715" + " ")
             # question mark
             cleanSummary = cleanSummary.replace(u"\u25EC", "?" + " ")
-            # replacing symbols and added padding for correct spacing
-            # check
-            cleanSummary = cleanSummary.replace(u"\u2705", u"\u2713" + " ")
-            # cross
-            cleanSummary = cleanSummary.replace(u"\u274c", u"\u2715" + " ")
-            # question mark
-            cleanSummary = cleanSummary.replace(u"\u25EC", "?" + " ")
 
-            posArray = [i for i in range(len(cleanSummary)) if cleanSummary.startswith("?", i)]
             posArray = [i for i in range(len(cleanSummary)) if cleanSummary.startswith("?", i)]
 
             for i in posArray:
                 x = i + 1
-            for i in posArray:
-                x = i + 1
-
                 while cleanSummary[x].isspace():
-                    x += 1
-                while cleanSummary[x].isspace():
-                    x += 1
-
-                while not cleanSummary[x].isspace():
                     x += 1
                 while not cleanSummary[x].isspace():
                     x += 1
-
                 cleanSummary = cleanSummary[:x] + cleanSummary[x + 1:]
-                cleanSummary = cleanSummary[:x] + cleanSummary[x + 1:]
-
-            return cleanSummary
             return cleanSummary
