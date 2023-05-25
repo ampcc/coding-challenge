850a0fb3619a8d950b395c96531b9fbfe6388da8
Lorenz Schreyer <lorenz.schreyer@gmail.com>
2023-05-25 12:31:22 +0200

    resetted db for tests

--- backend/codingchallenge/codingchallenge/settings.py
+++ backend/codingchallenge/codingchallenge/settings.py
@@ -87,14 +87,21 @@ WSGI_APPLICATION = 'codingchallenge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'coding-challenge',
+#         'USER': 'amplimind',
+#         'PASSWORD': 'admin',
+#         'HOST': 'database',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'coding-challenge',
-        'USER': 'amplimind',
-        'PASSWORD': 'admin',
-        'HOST': 'database',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
