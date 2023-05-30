13685627e78b3b29853a9f0fef642072d629cb97
lorenz <lorenz.schreyer@gmail.com>
2023-05-30 22:07:51 +0200

    changed result of Mocklinter, fixed pep8 styleguide

--- backend/codingchallenge/api/tests/views/application/test_getResult.py
+++ backend/codingchallenge/api/tests/views/application/test_getResult.py
@@ -1,8 +1,6 @@
 from pathlib import Path
-from textwrap import dedent
-from unittest.mock import patch
-from django.conf import settings
 
+from django.conf import settings
 from rest_framework import status
 from rest_framework.test import APITransactionTestCase
 
@@ -11,21 +9,41 @@ from ....models.application import Application
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent
 
+
 # patch is used to bypass the default githubApi and to raplace the following methods with mock data
 class test_getResult(APITransactionTestCase):
     reset_sequences = True
     applicationUrl = "/api/admin/applications/"
     url = '/api/admin/applications/results/'
-    mockLinterResult = "+----SUMMARY----+------------+---------------+-------+-------+--------+--------------+\n| Descriptor    | Linter     | Mode          | Files | Fixed | Errors | Elapsed time |\n+---------------+------------+---------------+-------+-------+--------+--------------+\n| ✓  ACTION     | actionlint | list_of_files |     1 |       |      0 |        0.02s |\n| ✓  COPYPASTE  | jscpd      | project       |   n/a |       |      0 |        0.93s |\n| ✓  REPOSITORY | checkov    | project       |   n/a |       |      0 |       12.79s |\n| ✓  REPOSITORY | devskim    | project       |   n/a |       |      0 |        0.65s |\n| ✓  REPOSITORY | dustilock  | project       |   n/a |       |      0 |        0.02s |\n| ✓  REPOSITORY | gitleaks   | project       |   n/a |       |      0 |        0.11s |\n| ✓  REPOSITORY | git_diff   | project       |   n/a |       |      0 |        0.01s |\n| ✓  REPOSITORY | secretlint | project       |   n/a |       |      0 |        0.65s |\n| ✓  REPOSITORY | syft       | project       |   n/a |       |      0 |        0.43s |\n| ✓  REPOSITORY | trivy      | project       |   n/a |       |      0 |        3.41s |\n| ✕  SPELL      | cspell     | list_of_files |     2 |       |      2 |        2.85s |\n| ✓  SPELL      | misspell   | list_of_files |     1 |       |      0 |        0.04s |\n| ⁇ YAML        | prettier   | list_of_files |     1 |       |      1 |        0.42s |\n| ✓  YAML       | v8r        | list_of_files |     1 |       |      0 |        3.14s |\n| ✕  YAML       | yamllint   | list_of_files |     1 |       |      1 |        0.22s |\n+---------------+------------+---------------+-------+-------+--------+--------------+"
+    mockLinterResult = [
+        ['Descriptor', 'Linter', 'Mode', 'Files', 'Fixed', 'Errors', 'Elapsed time'],
+        ['✓ ACTION', 'actionlint', 'list_of_files', '1', '', '0', '0.02s'],
+        ['✓ COPYPASTE', 'jscpd', 'project', 'n/a', '', '0', '0.93s'],
+        ['✓ REPOSITORY', 'checkov', 'project', 'n/a', '', '0', '12.79s'],
+        ['✓ REPOSITORY', 'devskim', 'project', 'n/a', '', '0', '0.65s'],
+        ['✓ REPOSITORY', 'dustilock', 'project', 'n/a', '', '0', '0.02s'],
+        ['✓ REPOSITORY', 'gitleaks', 'project', 'n/a', '', '0', '0.11s'],
+        ['✓ REPOSITORY', 'git_diff', 'project', 'n/a', '', '0', '0.01s'],
+        ['✓ REPOSITORY', 'secretlint', 'project', 'n/a', '', '0', '0.65s'],
+        ['✓ REPOSITORY', 'syft', 'project', 'n/a', '', '0', '0.43s'],
+        ['✓ REPOSITORY', 'trivy', 'project', 'n/a', '', '0', '3.41s'],
+        ['✕ SPELL', 'cspell', 'list_of_files', '2', '', '2', '2.85s'],
+        ['✓ SPELL', 'misspell', 'list_of_files', '1', '', '0', '0.04s'],
+        ['? YAML', 'prettier', 'list_of_files', '1', '', '1', '0.42s'],
+        ['✓ YAML', 'v8r', 'list_of_files', '1', '', '0', '3.14s'],
+        ['✕ YAML', 'yamllint', 'list_of_files', '1', '', '1', '0.22s']
+    ]
 
     def setUp(self):
         # Authorization
         MockAuth.admin(self)
         settings.DEPLOY_OFFLINE = True
         # Create Challenge
-        self.client.post("/api/admin/challenges/",
-                         {"challengeHeading": "TestChallenge", "challengeText": "TestChallengeDescription"},
-                         format='json')
+        self.client.post(
+            "/api/admin/challenges/",
+            {"challengeHeading": "TestChallenge", "challengeText": "TestChallengeDescription"},
+            format='json'
+        )
 
         # Create Application
         self.applicationId = "appl0001"
@@ -55,6 +73,7 @@ class test_getResult(APITransactionTestCase):
 
         response = self.client.get(self.url + self.applicationId, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
     # Todo: should be implemented soon
     # def test_wrongStatus(self, mockGetRepoUrl, mockGetLinterLog):
     #     self.application.status = Application.Status.CHALLENGE_STARTED
@@ -76,5 +95,9 @@ class test_getResult(APITransactionTestCase):
 
         response = self.client.get(self.url + self.applicationId, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, {'githubUrl': "https://api.github.com/repos/ampcc/" + self.applicationId,
-                                         'content': self.mockLinterResult})
+        self.assertEqual(
+            response.data, {
+                'githubUrl': "https://api.github.com/repos/ampcc/" + self.applicationId,
+                'content': self.mockLinterResult
+            }
+        )
