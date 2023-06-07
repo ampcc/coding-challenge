33f5c99733132b8517f3e64e705b161e92b34183
Lorenz Schreyer <46090335+lorenzschreyer@users.noreply.github.com>
2023-06-07 14:55:54 +0200

    Update settings.py

--- backend/codingchallenge/codingchallenge/settings.py
+++ backend/codingchallenge/codingchallenge/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'codingchallenge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'coding-challenge',
-#         'USER': 'amplimind',
-#         'PASSWORD': 'admin',
-#         'HOST': 'database',
-#         'PORT': '5432'
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'coding-challenge',
+        'USER': 'amplimind',
+        'PASSWORD': 'admin',
+        'HOST': 'database',
+        'PORT': '5432'
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
