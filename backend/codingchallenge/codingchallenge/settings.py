d09d8b467e9a1214a4638add087bbed521164b0e
Marcodaum <marco.daum@schanzer-racing.de>
2023-06-12 20:21:04 +0200

    settings -> change to postgres again

--- backend/codingchallenge/codingchallenge/settings.py
+++ backend/codingchallenge/codingchallenge/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'codingchallenge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'coding-challenge',
@@ -100,14 +100,14 @@ WSGI_APPLICATION = 'codingchallenge.wsgi.application'
         'HOST': 'database',
         'PORT': '5432'
     }
-}'''
+}
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
