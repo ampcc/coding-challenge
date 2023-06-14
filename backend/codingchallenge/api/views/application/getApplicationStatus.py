a501c01c8a2463fd2970c888441a13ba63e93a24
lorenz <lorenz.schreyer@gmail.com>
2023-06-14 14:56:51 +0200

    imported missing jsonMessages

--- backend/codingchallenge/api/views/application/getApplicationStatus.py
+++ backend/codingchallenge/api/views/application/getApplicationStatus.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework.response import Response
+
+from ...include import jsonMessages
 from ...models import Application
 from ...serializers import GetApplicationStatus
 
