b5589c8e91a12876845a9623a0276f0e38d4c9cf
lorenz <lorenz.schreyer@gmail.com>
2023-06-14 15:10:15 +0200

    refactored applicant views

--- backend/codingchallenge/api/views/application/getApplicationStatus.py
+++ backend/codingchallenge/api/views/application/getApplicationStatus.py
@@ -7,6 +7,7 @@ from ...include import jsonMessages
 from ...models import Application
 from ...serializers import GetApplicationStatus
 
+
 def get(request):
     try:
         user = User.objects.get(username=request.user.username)
@@ -19,9 +20,15 @@ def get(request):
     try:
         application = Application.objects.get(applicationId=user.username)
     except Application.DoesNotExist:
-        return Response(jsonMessages.error_json_response("The referenced application can not be found!"), status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            jsonMessages.error_json_response("The referenced application can not be found!"),
+            status=status.HTTP_404_NOT_FOUND
+        )
     except Application.MultipleObjectsReturned:
-        return Response(jsonMessages.error_json_response("Multiple applications with the same id exist!"), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(
+            jsonMessages.error_json_response("Multiple applications with the same id exist!"),
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
 
     serializer = GetApplicationStatus(application, many=False)
     return Response(serializer.data, status=status.HTTP_200_OK)
--- backend/codingchallenge/api/views/application/loginWithApplicationKey.py
+++ backend/codingchallenge/api/views/application/loginWithApplicationKey.py
@@ -1,13 +1,16 @@
-import urllib.parse
 import time
+import urllib.parse
+
+from cryptography.fernet import Fernet
+from django.conf import settings
+from django.contrib.auth import authenticate
+from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from rest_framework import status
-from django.contrib.auth import authenticate
-from django.conf import settings
-from cryptography.fernet import Fernet
+
 from ...include import jsonMessages
 from ...models import Application
+
 
 def login(request, **kwargs):
     if kwargs.keys():
@@ -19,11 +22,17 @@ def login(request, **kwargs):
         try:
             decrypted_message = fernet.decrypt(key).decode()
         except:
-            return Response(jsonMessages.error_json_response(error_message_key_does_not_exist), status=status.HTTP_401_UNAUTHORIZED)
+            return Response(
+                jsonMessages.error_json_response(error_message_key_does_not_exist),
+                status=status.HTTP_401_UNAUTHORIZED
+            )
 
         if len(fernet_key) != 44:
-            return Response(jsonMessages.error_json_response(error_message_key_does_not_exist), status=status.HTTP_401_UNAUTHORIZED)
-        
+            return Response(
+                jsonMessages.error_json_response(error_message_key_does_not_exist),
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
         username = decrypted_message[0:8]
         password = decrypted_message[9:]
 
@@ -34,12 +43,20 @@ def login(request, **kwargs):
                 if user.application.status < Application.Status.ARCHIVED:
                     user.application.status = Application.Status.EXPIRED
                     user.save()
-                return Response(jsonMessages.error_json_response("The application is expired!"), status=status.HTTP_410_GONE)
+                return Response(
+                    jsonMessages.error_json_response("The application is expired!"),
+                    status=status.HTTP_410_GONE
+                )
             token, created = Token.objects.get_or_create(user=user)
-            return Response({
-                "token": token.key
-            })
+            return Response(
+                {
+                    "token": token.key
+                }
+            )
         else:
-            return Response(jsonMessages.error_json_response(error_message_key_does_not_exist), status=status.HTTP_401_UNAUTHORIZED)
-    
+            return Response(
+                jsonMessages.error_json_response(error_message_key_does_not_exist),
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
     return Response(jsonMessages.error_json_response("No key given!"), status=status.HTTP_422_UNPROCESSABLE_ENTITY)
